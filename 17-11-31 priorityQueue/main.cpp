8f90bf3e13f69a6c8ab573945885fadae79c5631
h57827581 <34902886+h57827581@users.noreply.github.com>
2017-12-28 10:39:34 +0800

    Add files via upload

---
+++ 17-11-31 priorityQueue/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+#include <time.h>
+#include "priorityQueue.h"
+using namespace std;
+
+int main()
+{
+	srand(time(0));
+
+	system("pause");
+	return 0;
+}
---
+++ 17-11-31 priorityQueue/priorityQueue.h
@@ -0,0 +1,15 @@
+#ifndef _priorityQueue
+#define _priorityQueue
+
+#include <iostream>
+tempplate <class T> class priorityQueue
+{
+	public:
+
+	private:
+
+};
+
+
+#endif
+
